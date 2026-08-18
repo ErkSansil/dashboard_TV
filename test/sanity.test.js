0ed461d37c64cdcd771eb9a4d2bc6705b931c119
ErkSansil <ericksilvestre.amo@gmail.com>
2026-08-18 10:19:48 -0300

    chore: estrutura inicial do projeto e assets de marca

---
+++ test/sanity.test.js
@@ -0,0 +1,7 @@
+// test/sanity.test.js
+const test = require('node:test');
+const assert = require('node:assert/strict');
+
+test('ambiente de testes está funcionando', () => {
+  assert.equal(1 + 1, 2);
+});
