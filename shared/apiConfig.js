dcc1841ed281840ff4cb6fc3871ed9c7548aa9f3
ErkSansil <ericksilvestre.amo@gmail.com>
2026-08-18 12:49:23 -0300

    chore: url do apps script implantado

---
+++ shared/apiConfig.js
@@ -0,0 +1,5 @@
+var APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby6IdZpyiGxsnoNSURehhoWVYgHZ6-aul2kOcTCFIHEeXKaeVjnp3RA5FwpD2Lhwo_2/exec';
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { APPS_SCRIPT_URL: APPS_SCRIPT_URL };
+}
